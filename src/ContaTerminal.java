b7928b2284f4d4af8031a4a8c385a77c4263c515
kevinarau <153324385+kevinarau@users.noreply.github.com>
2023-12-30 11:05:38 -0300

    meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ContaTerminal {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Locale;
+import java.util.Scanner;
+             public class ContaTerminal {
+
+                 public static void main( String [] args   ) {
+
+                     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+                     System.out.println(" Por favor,  digite sua conta." );
+                     int numero  = scanner.nextInt();
+
+
+                     System.out.println(" Por favor, digite o numero sua agencia");
+                     String Agencia = scanner.next();
+
+
+                     System.out.println(" Digite seu nome" );
+                     String nome = scanner.next();
+
+                     System.out.println("Digite seu sobrenome");
+                     String sobrenome = scanner.next();
+
+
+                     System.out.println("Digite seu saldo" );
+                     double  Saldo = scanner.nextDouble();
+
+                     String mensagemAgradecimento = String.format("Olá kevin araujo, Obrigado por criar uma conta em nosso banco. Sua agência é % 132-4, sua conta é 1324  e seu saldo de 243,43 já está disponível para saque. 243,43",
+                             nome , sobrenome, Agencia, numero, Saldo );
+                     long agradecer = scanner.next();
+
+
+
+             System.out.println("Nome do Cliente " + nome +  sobrenome);
+             System.out.println("Conta  = "  + numero);
+             System.out.println("Agência = " + Agencia);
+             System.out.println(" Meu saldo = " + Saldo );
+             System.out.println("agradecer" + mensagemAgradecimento);
+
+
+         scanner.close();
+         }
     }
-}
