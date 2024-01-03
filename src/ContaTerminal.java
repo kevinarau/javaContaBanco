680f4a5b5b561f31a22081ef75b72f29786b6031
kevinarau <153324385+kevinarau@users.noreply.github.com>
2024-01-03 16:40:21 -0300

    meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,39 +1,30 @@
 
-import java.util.Locale;
 import java.util.Scanner;
-             public class ContaTerminal {
 
-                 public static void main( String [] args   ) {
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-                     Scanner scanner = new Scanner(System.in).useLocale (Locale.US);
+        // Solicitação e leitura dos dados da conta
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
 
+        System.out.println("Por favor, digite o número da Conta:");
+        String numeroConta = scanner.nextLine();
 
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
 
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
 
-                     System.out.println("Por favor, digite o número da Agência:");
-                     String agencia = scanner.nextLine();
+        // Mensagem de agradecimento com os dados da conta
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.";
 
-                     System.out.println("Por favor, digite o número da Conta:");
-                     int numero = scanner.nextInt();
-                     scanner.nextLine();
+        System.out.println(mensagem);
 
-                     System.out.println("Por favor, digite o nome do Cliente:");
-                     String nomeCliente = scanner.nextLine();
-
-                     System.out.println("Por favor, digite o saldo:");
-                     double saldo = scanner.nextDouble();
-
-
-
-                     ContaBanco conta = new ContaBanco(numero, agencia, nomeCliente, saldo);
-
-                     // Exibindo mensagem com os detalhes da conta
-                     System.out.println("Olá " + conta.getNomeCliente()+ ", obrigado por criar uma conta em nosso banco, sua agência é "
-                             + conta.getAgencia() + ", conta " + conta.getNumero() + " e seu saldo " + conta.getSaldo()
-                             + " já está disponível para saque.");
-
-
-
-         scanner.close();
-         }
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
     }
+}
